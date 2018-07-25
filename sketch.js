027ca391673c3adacb2e011d03605d7da54fb04a
Dannycool <danielbayode9@gmail.com>
2018-07-25 10:55:11 -0400

    danny is cool

---
+++ sketch.js
@@ -0,0 +1,43 @@
+function setup(){
+    createCanvas(900,700);
+    background(100);
+}
+
+function draw(){
+    rect(450,600,50,50);
+    fill("blue");
+    rect(468,559,10,40);
+    function keyPressed() {
+        if (keyCode===LEFT_ARROW) {
+          value = 900;
+        } else if (keyCode===RIGHT_ARROW) {
+          value = 900;
+        }
+    }
+   ellipse(50,50,50,50);
+   ellipse(150,50,50,50);
+   ellipse(250,50,50,50);
+   ellipse(350,50,50,50);
+   ellipse(450,50,50,50);
+   ellipse(550,50,50,50);
+   ellipse(650,50,50,50);
+   ellipse(750,50,50,50);
+   ellipse(850,50,50,50);
+   ellipse(50,150,50,50);
+   ellipse(150,150,50,50);
+   ellipse(250,150,50,50);
+   ellipse(350,150,50,50);
+   ellipse(450,150,50,50);
+   ellipse(550,150,50,50);
+   ellipse(650,150,50,50);
+   ellipse(750,150,50,50);
+   ellipse(850,150,50,50);
+
+
+
+
+
+
+
+}
+
